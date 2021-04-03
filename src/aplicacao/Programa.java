efbce2a24c4b69d7e4b7f25f508d5407a78ba91a
sourcegilmar <gilmardw@gmail.com>
2021-04-03 16:11:03 -0300

    Implementacao da classe programa

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,24 @@
 package aplicacao;
 
+import javax.swing.JOptionPane;
+
+import entidades.Singleton;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		
+		Singleton sing1;
+		Singleton sing2;
+
+		sing1 = Singleton.getSingletonInstance();
+		sing2 = Singleton.getSingletonInstance();
+
+		if (sing1.equals(sing2)) {
+			JOptionPane.showMessageDialog(null,
+					"ATENÇÃO!" + "equals >> singleton1 e singleton2" + "se referem a mesma instância de Singleton");
+			System.out.println("singleton1 " + sing1.hashCode() + " e singleton2 " + sing2.hashCode()
+					+ " se referem a mesma instância de Singleton");
+		}
 	}
 	
 }
